f40269f82f59715cb2bb411847a9d4bfc0bf620e
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-05-20 08:50:32 -0600

    Ejercicio_5.py

--- Ejercicios_repaso/Ejercicio_5.py
+++ Ejercicios_repaso/Ejercicio_5.py
@@ -5,5 +5,6 @@ n2_e = int(num2)
 
 print(f"Los numeros entre el numero {n1_e} y el numero {n2_e} son:")
 i=1
+#n1_e+1 para que no imprima el primer numero ingresado
 for i in range (n1_e+1,n2_e):
     print(i)

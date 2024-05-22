6766ba91a214ddaa653cc98a861dab11790b15e5
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-05-21 18:23:46 -0600

    .

--- Parcial1/Parcial1/Ejercicios_repaso/Ejercicio_10.py
+++ Parcial1/Parcial1/Ejercicios_repaso/Ejercicio_10.py
@@ -1,5 +1,19 @@
-i=1  
+i=1 
+calf=0 
+reprobado=0
+aprobado=0
+cantA=0
+cantR=0
 
 for i in range(1,16):
-    num=(input(f"Ingrese la calificación del alumno {i}"))
-    print("@")
+
+    calf=int(input(f"Ingrese la calificación del alumno {i}: "))
+
+    #contador de aprobados y reprobados
+    if calf>=60:
+        aprobado=aprobado+1
+    else:
+        reprobado=reprobado+1
+
+print(f"La cantidad de aprobados es: {aprobado}")
+print(f"La cantidad de reprobados es: {reprobado}")

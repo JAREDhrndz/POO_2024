6112d2d3c2dcc44cb449f933beaa51afa2db940d
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-05-23 10:38:15 -0600

    .

---
+++ Examen_rapido/examen_1.py
@@ -0,0 +1,39 @@
+resp="SI"
+numP=0
+while resp=="SI":
+    nom = input("Nombre del paciente: ")
+    tipoS = input("Tipo de sangre: ")
+
+    #SISTOLICA
+    si1 = int(input("Primera medición SIStolica: "))
+    si2 = int(input("Segunda medición SIStolica: "))
+    si3 = int(input("Tercera medición SIStolica: "))
+
+    #DIASTOLICA
+    dia1 = int(input("Primera medición DIAstolica: "))
+    dia2 = int(input("Segunda medición DIAstolica: "))
+    dia3 = int(input("Tercera medición DIAstolica: "))
+
+    #FINAL
+    fin = int(input("Medición final del dia: "))
+
+    #CALCULAR
+    parcialSI=(si1+si2+si3)/3
+    parcialDIA=(dia1+dia2+dia3)/3
+    final=(parcialSI+parcialDIA+fin)/3
+    
+    #IMPRIMIR
+    input(f"El primedio parcial de presión SIStolica es {parcialSI}")
+    input(f"El primedio parcial de presión DIAstolica es {parcialDIA}")
+    input(f"La presión final total es {final}")
+
+    #CONDICIONAL
+    if parcialSI<=120 or parcialDIA<=80:
+        print("Tiene una presión normal")
+    else:
+        print("Tiene una presion anormal")
+
+    resp = input("Desea ingresar otro paciente? ")
+    numP=numP+1
+
+print(f"Los pacientes capturados fueron: {numP}")

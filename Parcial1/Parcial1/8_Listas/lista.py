beffe46792589b4749516062e8d0872a0e0e42d8
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-06-04 10:58:52 -0600

    .

---
+++ Parcial1/Parcial1/8_Listas/lista.py
@@ -0,0 +1,29 @@
+#Crear una lista con valores numericos enteros e imprimir la lista
+
+
+numeros=[23,34]
+
+print(numeros)
+
+
+#for i in numeros:
+    #print(i)
+
+i=0
+
+while i < len(numeros):
+    print(numeros[i])
+    i+=1
+
+
+
+#crear una lista de palabras, posteriormente ingresar una palabra para 
+#buscar la coincidencia en la lista e indicar la posicion de la palabra
+#en caso contrario indiciar 1 vez si no la encontró
+palabras["hola","2024","10.23","True"]
+buscar_p=input=("Ingres a buscar")
+
+
+while i < len(numeros):
+    print(numeros[i])
+    i+=1

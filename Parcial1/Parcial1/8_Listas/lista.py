818d918b48bd50aaeace4cd368fa072e45891b9f
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-06-05 09:48:17 -0600

    .

--- Parcial1/Parcial1/8_Listas/lista.py
+++ Parcial1/Parcial1/8_Listas/lista.py
@@ -1,8 +1,6 @@
 #Crear una lista con valores numericos enteros e imprimir la lista
 
-
 numeros=[23,34]
-
 print(numeros)
 
 
@@ -15,15 +13,28 @@ while i < len(numeros):
     print(numeros[i])
     i+=1
 
+#crear una lista de palabras, posteriormente ingresar una palabra 
+#para buscar la coincidencia en la lista e indicar la posicion de 
+#la palabra en caso contrario indiciar 1 vez si no la encontró.
 
+# palabras=["hola","2024","10.23","True"]
+# buscar_p=input("Ingrese la palabra a buscar: ")
 
-#crear una lista de palabras, posteriormente ingresar una palabra para 
-#buscar la coincidencia en la lista e indicar la posicion de la palabra
-#en caso contrario indiciar 1 vez si no la encontró
-palabras["hola","2024","10.23","True"]
-buscar_p=input=("Ingres a buscar")
+# no=True
+# for i in palabras:
+#     if buscar_p==i:
+#         print(f"Se encontro la palabra {buscar_p} en la posición: {palabras.index(i)}")
+#         no=False
 
+# if no:
+#     print(f"No se encontró la palabra")
 
-while i < len(numeros):
+#CON WHILE
+palabras=["hola","2024","10.23","True"]
+buscar_p=input("Ingrese la palabra a buscar: ")
+
+i=0
+
+while i == len(buscar_p):
     print(numeros[i])
     i+=1

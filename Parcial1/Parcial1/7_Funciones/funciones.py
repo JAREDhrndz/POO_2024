b7e76cf5721f6e3d699983cc78b9dd77275afd8c
Jared Alonso Hernandez Ortega <jaredhdz5834@gmail.com>
2024-06-06 10:12:42 -0600

    .

--- Parcial1/Parcial1/7_Funciones/funciones.py
+++ Parcial1/Parcial1/7_Funciones/funciones.py
@@ -1,39 +1,39 @@
 #Funcion que no recibe parametros y no regresa valor
-def sumaNum1():
+# def sumaNum1():
 
-    n1=int(input("Numero 1: "))
-    n2=int(input("Numero 2: "))
-    suma=n1+n2
-    print(f"{n1}+{n2}={suma}")
+#     n1=int(input("Numero 1: "))
+#     n2=int(input("Numero 2: "))
+#     suma=n1+n2
+#     print(f"{n1}+{n2}={suma}")
 
-#sumaNum1()
-
-
-#Funcion que no recibe parametros y regresa valor
-def sumaNum2():
-
-    n1=int(input("Numero 1: "))
-    n2=int(input("Numero 2: "))
-    suma=n1+n2
-    return (f"{n1}+{n2}={suma}")
-
-#print(sumaNum2())
+# #sumaNum1()
 
 
-#Funcion que recibe parametros y no regresa valor
-def sumaNum3(n1,n2):
+# #Funcion que no recibe parametros y regresa valor
+# def sumaNum2():
 
-    suma=n1+n2
-    print(f"{n1}+{n2}={suma}")
+#     n1=int(input("Numero 1: "))
+#     n2=int(input("Numero 2: "))
+#     suma=n1+n2
+#     return (f"{n1}+{n2}={suma}")
 
-#num1=int(input(Numero 1: ))
-#num2=int(input(Numero 2: ))
-sumaNum3(num1,num2)
+# #print(sumaNum2())
 
-#Funcion que recibe parametros y regresa valor
-def sumaNum4(n1,n2):
-    suma=n1+n2
-    return (f"{n1}+{n2}={suma}")
+
+# #Funcion que recibe parametros y no regresa valor
+# def sumaNum3(n1,n2):
+
+#     suma=n1+n2
+#     print(f"{n1}+{n2}={suma}")
+
+# #num1=int(input(Numero 1: ))
+# #num2=int(input(Numero 2: ))
+# sumaNum3(num1,num2)
+
+# #Funcion que recibe parametros y regresa valor
+# def sumaNum4(n1,n2):
+#     suma=n1+n2
+#     return (f"{n1}+{n2}={suma}")
 
 #num1=int(input(Numero 1: ))
 #num2=int(input(Numero 2: ))
@@ -43,3 +43,10 @@ def sumaNum4(n1,n2):
 #--------TAREA---------
 #crear un programa que solicite a traves de una función la siguiente informacion: 
 #nombre del paciente, edad, estatura, tipo de sangre.
+
+
+
+
+
+
+
--- Parcial1/Parcial1/8_Listas/lista.py
+++ Parcial1/Parcial1/8_Listas/lista.py
@@ -1,40 +1,55 @@
-#Crear una lista con valores numericos enteros e imprimir la lista
+# #Crear una lista con valores numericos enteros e imprimir la lista
 
-numeros=[23,34]
-print(numeros)
+# numeros=[23,34]
+# print(numeros)
 
 
-#for i in numeros:
-    #print(i)
+# #for i in numeros:
+#     #print(i)
 
-i=0
+# i=0
 
-while i < len(numeros):
-    print(numeros[i])
-    i+=1
+# while i < len(numeros):
+#     print(numeros[i])
+#     i+=1
 
-#crear una lista de palabras, posteriormente ingresar una palabra 
-#para buscar la coincidencia en la lista e indicar la posicion de 
-#la palabra en caso contrario indiciar 1 vez si no la encontró.
+# #crear una lista de palabras, posteriormente ingresar una palabra 
+# #para buscar la coincidencia en la lista e indicar la posicion de 
+# #la palabra en caso contrario indiciar 1 vez si no la encontró.
 
+# # palabras=["hola","2024","10.23","True"]
+# # buscar_p=input("Ingrese la palabra a buscar: ")
+
+# # no=True
+# # for i in palabras:
+# #     if buscar_p==i:
+# #         print(f"Se encontro la palabra {buscar_p} en la posición: {palabras.index(i)}")
+# #         no=False
+
+# # if no:
+# #     print(f"No se encontró la palabra")
+
+# #CON WHILE
 # palabras=["hola","2024","10.23","True"]
 # buscar_p=input("Ingrese la palabra a buscar: ")
 
-# no=True
-# for i in palabras:
-#     if buscar_p==i:
-#         print(f"Se encontro la palabra {buscar_p} en la posición: {palabras.index(i)}")
-#         no=False
+# i=0
 
-# if no:
-#     print(f"No se encontró la palabra")
+# while i < len(buscar_p):
+#     print(numeros[i])
+#     i+=1
 
-#CON WHILE
-palabras=["hola","2024","10.23","True"]
-buscar_p=input("Ingrese la palabra a buscar: ")
+#crear una lista multidimensional (matriz) para crear una agenda
 
-i=0
+agenda=[
+ ["Carlos", 6181354710],
+ ["Dago", 6189895415],
+ ["Jared", 6181932110],
+]
 
-while i < len(buscar_p):
-    print(numeros[i])
-    i+=1
+for i in agenda:
+     print(f"{agenda.index(i)+1}.-{i}")
+    
+#crear un prgrama que ppermita gestionar  peliculas, colocar un menu de opciones para agregar, remover, consultar peliculas.
+# notas utilizar funciones y mandar llamar desde otro archivo 
+#utilizar linea para alamcenar los nombres de las peliculas 
